6dc84f50bc0bf7f8f9c09940d6f277e02fa58d99
Mike Ford <fordmichael20@gmail.com>
2024-09-28 13:11:39 -0400

    split core

--- finance/mpt_calc.py
+++ finance/mpt_calc.py
@@ -1,6 +1,3 @@
 import pandas as pd
 
-class returns:
-    min_date    = 
-    max_date    = 
-    periodicity = "monthly"
+
--- python/data_frame_utils.py
+++
@@ -1,5 +0,0 @@
-import polars as pol
-
-def df_dupe_checker(df, key_cols, return_type="dupes"):
-    
-    return None
